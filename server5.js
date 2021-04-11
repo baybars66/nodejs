b9681323f0e0a46acf8f033aac429945534b4a6a
baybars66 <bay66bars@gmail.com>
2021-04-11 11:15:45 +0300

    upp

--- server5.js
+++ server5.js
@@ -7,17 +7,17 @@ const path = require('path');
 yonveren.use("/public", expr.static("dene"));
 yonveren.use('/public', expr.static(path.join(__dirname, 'public')));
 
-const indexCon = (req, res) => { res.sendFile(path.join(__dirname, "index.html"));}
-const logCon = (req, res) => { res.sendFile(path.join(__dirname, "index2.html"));}
+//const indexCon = (req, res) => { res.sendFile(path.join(__dirname, "index.html"));}
+//const logCon = (req, res) => { res.sendFile(path.join(__dirname, "index2.html"));}
 
-yonveren.get ("/", indexCon);
-yonveren.get ("/log", logCon);
+//yonveren.get ("/", indexCon);
+//yonveren.get ("/log", logCon);
 
-/*
+
     yonveren.get ("/", (req, res) => 
     { res.sendFile(path.join(__dirname, "index.html"));
              
     });
 
-*/
+
 yonveren.listen (10068);
